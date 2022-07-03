9de918f9d0f47aa4d70f714fa8421463d3fafaee
jesse <szx961102@gmail.com>
2022-07-03 23:52:42 +0800

    update host

--- routes/nft.tsx
+++ routes/nft.tsx
@@ -1,6 +1,7 @@
 /** @jsx h */
 import { h } from "preact";
 import { PageProps, Handlers } from "$fresh/server.ts";
+import { tw } from 'twind'
 
 interface BaseResp<T> {
   code: number,
@@ -25,13 +26,20 @@ interface NftResp {
 
 export const handler: Handlers<BaseResp<BaseListResp<NftResp>> | null> = {
   async GET(_, ctx) {
-    const { username } = ctx.params;
-    const resp = await fetch(`https://fml233.cn:8443/api/nft/page`, {
+    const host = 'https://fml233.cn:8443/api';
+    const hostDev = 'http://localhost:9080/api';
+
+    const resp = await fetch(`${host}/nft/page`, {
       method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
       body: JSON.stringify({ page: 1, size: 50 })
     });
 
     const data = await resp.json();
+    console.log(data);
+
 
     return ctx.render(data);
   },
@@ -44,14 +52,17 @@ export default function Nft({ data }: PageProps<BaseResp<BaseListResp<NftResp>> 
   return (
     <div>
       <h1>NFT</h1>
-      {data.result?.records.map(nft => (
-        <div key={nft.tokenId}>
-          <img src={nft.image} width={64} height={64} />
-          <h1>{nft.name}</h1>
-          <p>{nft.nickname}</p>
+      <div class={tw`flex flex-wrap`}>
+        {data.result?.records.map(nft => (
+          <div key={nft.tokenId}>
+            <img src={nft.image} width={64} height={64} />
+            <h1>{nft.name}</h1>
+            <p>{nft.nickname}</p>
 
-        </div>
-      ))}
+          </div>
+        ))}
+      </div>
+
     </div>
   )
 
