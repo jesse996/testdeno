d0204f389c213be592be6d6002bede79ca9a500f
jesse <szx961102@gmail.com>
2022-07-04 15:55:21 +0800

    add daisy

--- components/layout/Container.tsx
+++ components/layout/Container.tsx
@@ -13,11 +13,11 @@ export type Props = {
 export const Container = ({ children, ...customMeta }: Props) => {
   return (
     <>
-      <div style={{ minHeight: "100vh" }}>
-        <Seo {...customMeta} />
-        <div className="container">{children}</div>
-        <div>foot</div>
-      </div>
+      {/* <div style={{ minHeight: "100vh" }}> */}
+      <Seo {...customMeta} />
+      <div className="container">{children}</div>
+      {/* </div> */}
+      <Footer />
     </>
   );
 };
@@ -40,3 +40,28 @@ const Seo = ({ ...customMeta }) => {
     </Head>
   );
 };
+
+const Footer = () => {
+  return <footer class="footer p-10 bg-neutral text-neutral-content">
+    <div>
+      <span class="footer-title">Services</span>
+      <a class="link link-hover">Branding</a>
+      <a class="link link-hover">Design</a>
+      <a class="link link-hover">Marketing</a>
+      <a class="link link-hover">Advertisement</a>
+    </div>
+    <div>
+      <span class="footer-title">Company</span>
+      <a class="link link-hover">About us</a>
+      <a class="link link-hover">Contact</a>
+      <a class="link link-hover">Jobs</a>
+      <a class="link link-hover">Press kit</a>
+    </div>
+    <div>
+      <span class="footer-title">Legal</span>
+      <a class="link link-hover">Terms of use</a>
+      <a class="link link-hover">Privacy policy</a>
+      <a class="link link-hover">Cookie policy</a>
+    </div>
+  </footer>
+}
--- routes/about.tsx
+++ routes/about.tsx
@@ -1,13 +1,33 @@
 /** @jsx h */
 import { h } from "preact";
 import { Handlers } from "$fresh/server.ts";
+import { Container } from "../components/layout/Container.tsx";
 
 export default function AboutPage() {
   return (
-    <main>
+    <Container>
       <h1>About</h1>
       <p>This is the about page.</p>
-    </main>
+      <div>
+        <a href="#my-modal-2" class="btn">open modal</a>
+        <div class="modal" id="my-modal-2">
+          <div class="modal-box">
+            <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
+            <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+            <div class="modal-action">
+              <a href="#" class="btn">Yay!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mockup-phone">
+        <div class="camera"></div>
+        <div class="display">
+          <div class="artboard artboard-demo phone-1">Hi. i am jesse</div>
+        </div>
+      </div>
+
+    </Container>
   )
 }
 
